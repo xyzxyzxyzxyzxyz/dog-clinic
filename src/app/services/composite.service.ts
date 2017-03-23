9dbd75906970a9569836150a786fb930c0d00308
XYZ <idm.caz@mailinator.com>
2017-03-23 16:09:09 +0100

    should return dogs with owners. Test pass

--- src/app/services/composite.service.ts
+++ src/app/services/composite.service.ts
@@ -6,9 +6,16 @@ import {DogDetail} from "../model/dog-detail";
 @Injectable()
 export class CompositeService {
 
-  constructor(dogsService: DogsService, ownerService: OwnerService) { }
+  constructor(private dogsService: DogsService, private ownerService: OwnerService) { }
 
   getDogs(): DogDetail[]{
-    return null;
+    let dogs = this.dogsService.getDogs();
+    let ownerIds = dogs.map(dog => dog.ownerId)
+    let owners = this.ownerService.getOwners(ownerIds);
+
+    return dogs.map(dog => {
+      let owner = owners.find(owner => owner.id == dog.ownerId);
+      return new DogDetail(dog, owner);
+    });
   }
 }
