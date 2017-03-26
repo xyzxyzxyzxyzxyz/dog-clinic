2cf061ca106104627193a78ca92e02e94a78d942
XYZ <idm.caz@mailinator.com>
2017-03-26 23:52:05 +0200

    No event is emitted if there is no dogDetail or appointment Passing test

--- src/app/components/dog-panel.component/dog-panel/dog-panel.component.spec.ts
+++ src/app/components/dog-panel.component/dog-panel/dog-panel.component.spec.ts
@@ -1,6 +1,8 @@
 import { DogPanelComponent } from './dog-panel.component';
 import {DogDetail} from "../../../model/dog-detail";
 import {Appointment} from "../../../model/appointment";
+import {Dog} from "../../../model/dog";
+import {Owner} from "../../../model/owner";
 
 describe('DogPanelComponent', () => {
   let component: DogPanelComponent;
@@ -9,15 +11,11 @@ describe('DogPanelComponent', () => {
     component = new DogPanelComponent();
   });
 
-  it('No event is emitted if there is no dogDetail or appointment', () => {
+  it('No event is emitted if there is no dogDetail', () => {
+    component.dogDetail = null;
+    component.nextAppointment = new Appointment(1, new Date());
 
-    let dogDetail:DogDetail = null;
-    let appointment:Appointment = null;
     let emittedAppointment:Boolean = false;
-
-    component.dogDetail = dogDetail;
-    component.nextAppointment = appointment;
-
     component.sendReminderButtonClicked.subscribe((appointment:Appointment) =>
       {
         emittedAppointment = true;
@@ -27,4 +25,24 @@ describe('DogPanelComponent', () => {
 
     expect(emittedAppointment).toBe(false);
   });
+
+  it('No event is emitted if there is no appointment', () => {
+    component.dogDetail = new DogDetail(
+      new Dog("Snoopy", 3, 1),
+      new Owner(1, "Charlie", "Brown")
+    );
+    component.nextAppointment = null;
+
+    let emittedAppointment:Boolean = false;
+    component.sendReminderButtonClicked.subscribe((appointment:Appointment) =>
+    {
+      emittedAppointment = true;
+    });
+
+    component.sendReminderButtonOnClick()
+
+    expect(emittedAppointment).toBe(false);
+  });
+
+
 });
